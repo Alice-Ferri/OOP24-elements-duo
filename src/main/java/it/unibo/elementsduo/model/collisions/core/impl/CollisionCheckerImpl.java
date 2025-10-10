bc767bf9dff39146245cc0c05f966bdff5807bab
christian <kadiuchristian8@gmail.com>
2025-10-10 11:44:41 +0200

    implemented CollisionChecker interface

---
+++ src/main/java/it/unibo/elementsduo/model/collisions/core/impl/CollisionCheckerImpl.java
@@ -0,0 +1,21 @@
+package it.unibo.elementsduo.model.collisions.core.impl;
+
+import java.util.List;
+
+import it.unibo.elementsduo.model.collisions.core.api.Collidable;
+import it.unibo.elementsduo.model.collisions.core.api.CollisionChecker;
+
+public class CollisionCheckerImpl implements CollisionChecker {
+
+    @Override
+    public void checkCollisions(List<Collidable> entities) {
+        for (int i = 0; i < entities.size(); i++) {
+            for (int k = i + 1; k < entities.size(); k++) {
+                if (entities.get(i).getHitBox().intersects(entities.get(k).getHitBox())) {
+                    System.out.println("collision detected");
+                }
+            }
+        }
+    }
+
+}
