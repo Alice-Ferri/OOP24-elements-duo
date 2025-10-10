fd04e43884d466a13b7a95415b62ed21edf7e2f3
christian <kadiuchristian8@gmail.com>
2025-10-10 16:34:32 +0200

    calculate penetration of two objects in a collision

--- src/main/java/it/unibo/elementsduo/model/collisions/core/impl/CollisionCheckerImpl.java
+++ src/main/java/it/unibo/elementsduo/model/collisions/core/impl/CollisionCheckerImpl.java
@@ -12,7 +12,28 @@ public class CollisionCheckerImpl implements CollisionChecker {
         for (int i = 0; i < entities.size(); i++) {
             for (int k = i + 1; k < entities.size(); k++) {
                 if (entities.get(i).getHitBox().intersects(entities.get(k).getHitBox())) {
-                    System.out.println("collision detected");
+
+                    // calcolo la distanza tra i due centri degli oggetti coinvolti lungo i due assi
+                    double dx = entities.get(i).getHitBox().getCenter().x()
+                            - entities.get(k).getHitBox().getCenter().x();
+                    double dy = entities.get(i).getHitBox().getCenter().y()
+                            - entities.get(k).getHitBox().getCenter().y();
+
+                    // Calcolo la sovrapposizione dei due corpi sui 2 assi
+                    double px = (entities.get(i).getHitBox().getHalfWidth()
+                            + entities.get(k).getHitBox().getHalfWidth()) - Math.abs(dx);
+                    double py = (entities.get(i).getHitBox().getHalfHeight()
+                            + entities.get(k).getHitBox().getHalfHeight()) - Math.abs(dy);
+
+                    // campo che usiamo per indicare di quanto i due oggettti si sono intersecati,
+                    // lungo l'asse in cui si sono intersecati di meno
+                    // così sappiamo di quanto andrà riposizionato il corpo
+                    double penetration;
+
+                    if (px < py)
+                        penetration = px;
+                    else
+                        penetration = py;
                 }
             }
         }
---
+++ src/main/java/it/unibo/elementsduo/resources/Vector2D.java
@@ -0,0 +1,5 @@
+package it.unibo.elementsduo.resources;
+
+public record Vector2D(double x, double y) {
+
+}
