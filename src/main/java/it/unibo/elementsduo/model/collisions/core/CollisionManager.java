909f05eae60e3e4530d32ac987bae3847274de84
christian <kadiuchristian8@gmail.com>
2025-10-10 11:10:04 +0200

    change CollisionManager

--- src/main/java/it/unibo/elementsduo/model/collisions/core/CollisionManager.java
+++ src/main/java/it/unibo/elementsduo/model/collisions/core/CollisionManager.java
@@ -2,14 +2,14 @@ package it.unibo.elementsduo.model.collisions.core;
 
 import java.util.List;
 
+/* class to manage collisions */
+
 public class CollisionManager {
     public void checkCollisions(List<Collidable> entities) {
         for (int i = 0; i < entities.size(); i++) {
-            for (int k = 0; k < entities.size(); k++) {
-                if (i != k) {
-                    if (entities.get(i).getHitBox().intersects(entities.get(k).getHitBox())) {
-                        System.out.println("collision detected");
-                    }
+            for (int k = i + 1; k < entities.size(); k++) {
+                if (entities.get(i).getHitBox().intersects(entities.get(k).getHitBox())) {
+                    System.out.println("collision detected");
                 }
             }
         }
