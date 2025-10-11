4db6055aa1bb429763686ed8b68070241eb5c076
christian <kadiuchristian8@gmail.com>
2025-10-11 16:41:20 +0200

    change CollisionChecker

--- src/main/java/it/unibo/elementsduo/model/collisions/core/api/CollisionChecker.java
+++ src/main/java/it/unibo/elementsduo/model/collisions/core/api/CollisionChecker.java
@@ -1,16 +1,9 @@
 package it.unibo.elementsduo.model.collisions.core.api;
 
 import java.util.List;
-import java.util.Optional;
-
-import it.unibo.elementsduo.resources.Vector2D;
 
 public interface CollisionChecker {
 
     public void checkCollisions(List<Collidable> entities);
-
-    public Optional<Double> getPenetration(Collidable objectA, Collidable objectB);
-
-    public Optional<Vector2D> getNormal(Collidable objectA, Collidable objectB);
 
 }
