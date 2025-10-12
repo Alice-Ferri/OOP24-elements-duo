0570f29d0ed79c9de0240d7825e417901265eba5
christian <kadiuchristian8@gmail.com>
2025-10-12 16:25:42 +0200

    create Lever class

---
+++ src/main/java/it/unibo/elementsduo/model/obstacles/InteractiveObstacles/impl/Lever.java
@@ -0,0 +1,33 @@
+package it.unibo.elementsduo.model.obstacles.InteractiveObstacles.impl;
+
+import it.unibo.elementsduo.model.collisions.core.api.Collidable;
+import it.unibo.elementsduo.model.collisions.hitbox.api.HitBox;
+import it.unibo.elementsduo.model.obstacles.InteractiveObstacles.api.Triggerable;
+
+public class Lever implements Triggerable, Collidable {
+
+    @Override
+    public HitBox getHitBox() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHitBox'");
+    }
+
+    @Override
+    public boolean isActive() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isActive'");
+    }
+
+    @Override
+    public void activate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'activate'");
+    }
+
+    @Override
+    public void deactivate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deactivate'");
+    }
+
+}
