41ae8e5ca1e455265d36e99c97d4f2c7029978fd
christian <kadiuchristian8@gmail.com>
2025-10-12 17:42:11 +0200

    create PushBox class

---
+++ src/main/java/it/unibo/elementsduo/model/obstacles/InteractiveObstacles/impl/PushBox.java
@@ -0,0 +1,13 @@
+package it.unibo.elementsduo.model.obstacles.InteractiveObstacles.impl;
+
+import it.unibo.elementsduo.model.obstacles.InteractiveObstacles.api.Pushable;
+
+public class PushBox implements Pushable {
+
+    @Override
+    public void push() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    }
+
+}
