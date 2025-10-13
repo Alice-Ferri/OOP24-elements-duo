a4ede28a50679623d759511de35391dcbe1ebc23
christian <kadiuchristian8@gmail.com>
2025-10-13 09:38:17 +0200

    implemented button class

--- src/main/java/it/unibo/elementsduo/model/obstacles/InteractiveObstacles/impl/button.java
+++ src/main/java/it/unibo/elementsduo/model/obstacles/InteractiveObstacles/impl/button.java
@@ -1,25 +1,37 @@
 package it.unibo.elementsduo.model.obstacles.InteractiveObstacles.impl;
 
+import it.unibo.elementsduo.model.collisions.core.api.Collidable;
+import it.unibo.elementsduo.model.collisions.hitbox.api.HitBox;
 import it.unibo.elementsduo.model.obstacles.InteractiveObstacles.api.Triggerable;
 
-public class button implements Triggerable {
+public class Button implements Triggerable, Collidable {
+
+    private boolean active;
+    private HitBox hitbox;
+
+    public Button(HitBox hitbox) {
+        this.hitbox = hitbox;
+        this.active = false;
+    }
 
     @Override
     public boolean isActive() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isActive'");
+        return this.active;
     }
 
     @Override
     public void activate() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'activate'");
+        this.active = true;
     }
 
     @Override
     public void deactivate() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deactivate'");
+        this.active = false;
+    }
+
+    @Override
+    public HitBox getHitBox() {
+        return this.hitbox;
     }
 
 }
