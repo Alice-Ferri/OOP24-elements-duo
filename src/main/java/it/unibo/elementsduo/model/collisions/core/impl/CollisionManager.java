62106a92ad693d19e5a3a5570b716c0160b90af9
christian <kadiuchristian8@gmail.com>
2025-10-13 16:45:04 +0200

    add other collisions cases

--- src/main/java/it/unibo/elementsduo/model/collisions/core/impl/CollisionManager.java
+++ src/main/java/it/unibo/elementsduo/model/collisions/core/impl/CollisionManager.java
@@ -5,6 +5,8 @@ import java.util.List;
 import it.unibo.elementsduo.model.collisions.core.api.Collidable;
 import it.unibo.elementsduo.model.collisions.core.api.CollisionChecker;
 import it.unibo.elementsduo.model.collisions.core.api.CollisionInformations;
+import it.unibo.elementsduo.model.enemies.api.Enemy;
+import it.unibo.elementsduo.model.obstacles.InteractiveObstacles.impl.Lever;
 import it.unibo.elementsduo.model.player.api.Player;
 import it.unibo.elementsduo.resources.Vector2D;
 
@@ -17,7 +19,7 @@ public class CollisionManager {
         List<CollisionInformations> ci = ck.checkCollisions(entities);
         for (CollisionInformations c : ci) {
             Player player = null;
-            Collidable other;
+            Collidable other = null;
             Collidable a = c.getObjectA();
             Collidable b = c.getObjectB();
             Vector2D normal = c.getNormal();
@@ -35,17 +37,23 @@ public class CollisionManager {
             if (player == null)
                 continue;
 
-            final double corrX = normal.getX() * c.getPenetration();
-            final double corrY = normal.getY() * c.getPenetration();
+            if (other instanceof Enemy) {
+                System.out.println("collision between player and enemy, game finish");
+            } else if (other instanceof Lever) {
+                // attiva leva
+            } else {
+                final double corrX = normal.getX() * c.getPenetration();
+                final double corrY = normal.getY() * c.getPenetration();
 
-            // orizzontal correction
-            player.move(corrX);
-            // vertical correction
-            if (normal.getY() == 1) {
-                player.landOn(player.getY() + corrY);
-            }
-            if (normal.getY() == -1) {
-                player.stopJump(player.getY() + corrY);
+                // orizzontal correction
+                player.move(corrX);
+                // vertical correction
+                if (normal.getY() == 1) {
+                    player.landOn(player.getY() + corrY);
+                }
+                if (normal.getY() == -1) {
+                    player.stopJump(player.getY() + corrY);
+                }
             }
         }
     }
