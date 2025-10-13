8ec34a9e307d6ef2ca0207388c8948061961f0ad
Leonardo <st10151@iismarconipieralisi.it>
2025-10-14 00:37:43 +0200

    init main controller

--- src/main/java/it/unibo/elementsduo/controller/impl/MainControllerImpl.java
+++ src/main/java/it/unibo/elementsduo/controller/impl/MainControllerImpl.java
@@ -2,6 +2,7 @@ package it.unibo.elementsduo.controller.impl;
 
 import javax.swing.SwingUtilities;
 
+import it.unibo.elementsduo.controller.gameLoop;
 import it.unibo.elementsduo.controller.api.MainController;
 import it.unibo.elementsduo.model.enemies.impl.EnemyFactoryImpl;
 import it.unibo.elementsduo.model.map.api.Level;
@@ -10,6 +11,23 @@ import it.unibo.elementsduo.model.obstacles.impl.obstacleFactory;
 import it.unibo.elementsduo.view.GameFrame;
 
 public class MainControllerImpl implements MainController {
+
+    private final Level level;
+    private final GameFrame view;
+    private final gameLoop gameLoop;
+
+    public MainControllerImpl(){
+
+        final MapLoader mapLoader = new MapLoader(new obstacleFactory(), new EnemyFactoryImpl());
+        try {
+            this.level = mapLoader.loadLevel(1);
+        } catch (final Exception e) {
+            throw new IllegalStateException("Impossibile caricare il livello.", e);
+        }
+        
+        this.view = new GameFrame(level);
+        this.gameLoop = new gameLoop();
+    }
 
     @Override
     public void update(double deltaTime) {
