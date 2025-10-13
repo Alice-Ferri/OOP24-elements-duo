83b3ff155d1578fb0dfce35d4a37812577052ce2
steppo <stefano.distefano3@studio.unibo.it>
2025-10-14 00:47:43 +0200

    implement run method in GameLoop

--- src/main/java/it/unibo/elementsduo/controller/GameLoop.java
+++ src/main/java/it/unibo/elementsduo/controller/GameLoop.java
@@ -1,10 +1,19 @@
 package it.unibo.elementsduo.controller;
 
+import java.util.Objects;
+
+import it.unibo.elementsduo.controller.api.MainController;
+
 public class GameLoop implements Runnable{
     private static final int TARGET_FPS = 60;
     private static final long OPTIMAL_TIME = 1_000_000_000/TARGET_FPS;
     private volatile boolean running = false;
     private Thread gameThread;
+    private final MainController engine;
+
+    public GameLoop (final MainController engine) {
+        this.engine = Objects.requireNonNull(engine);
+    }
 
     public synchronized void start () {
         if (running) {
@@ -36,7 +45,26 @@ public class GameLoop implements Runnable{
 
     @Override
     public void run() {
-        
+        long lastLoopTime = System.nanoTime();
+
+        while (running){
+            final long now = System.nanoTime();
+            final long UpdateLength = now - lastLoopTime;
+            lastLoopTime = now;
+            final double deltaTime = UpdateLength / ((double)OPTIMAL_TIME);
+            this.engine.update(deltaTime);
+            this.engine.render();
+
+            try {
+                final long sleepTime = (lastLoopTime - System.nanoTime() + OPTIMAL_TIME / 1_000_000);
+                if (sleepTime > 0) {
+                    Thread.sleep(sleepTime);
+                }
+            } catch (final InterruptedException e) {
+                Thread.currentThread().interrupt();
+                running=false;
+            }
+        }
     }
     
 }
