99713f976f41005bc01fc2dd4ada5e05cbafe04a
christian <kadiuchristian8@gmail.com>
2025-10-15 17:53:18 +0200

    add CollisionHandler interface

---
+++ src/main/java/it/unibo/elementsduo/model/collisions/core/api/CollisionHandler.java
@@ -0,0 +1,5 @@
+package it.unibo.elementsduo.model.collisions.core.api;
+
+public interface CollisionHandler {
+    void handle(CollisionInformations c);
+}
---
+++ src/main/java/it/unibo/elementsduo/model/obstacles/StaticObstacles/api /obstacle.java
@@ -0,0 +1,5 @@
+import it.unibo.elementsduo.model.collisions.core.api.Collidable;
+
+public interface obstacle extends Collidable {
+
+}
