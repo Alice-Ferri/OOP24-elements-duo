b38bd9a6f400f59df52a990f5e6c69b67638e5d7
christian <kadiuchristian8@gmail.com>
2025-10-20 22:59:52 +0200

    change pushable interface and implement a class for a pushable box in the game

--- src/main/java/it/unibo/elementsduo/model/obstacles/InteractiveObstacles/impl/PushBox.java
+++ src/main/java/it/unibo/elementsduo/model/obstacles/InteractiveObstacles/impl/PushBox.java
@@ -1,19 +1,35 @@
 package it.unibo.elementsduo.model.obstacles.InteractiveObstacles.impl;
 
-import it.unibo.elementsduo.model.collisions.hitbox.api.HitBox;
 import it.unibo.elementsduo.model.obstacles.InteractiveObstacles.api.Pushable;
 import it.unibo.elementsduo.resources.Position;
+import it.unibo.elementsduo.resources.Vector2D;
 
 public class PushBox extends InteractiveObstacle implements Pushable {
 
-    public PushBox(Position center, double halfWidth, double halfHeight) {
+    private final static double FRICTION = 0.9;
+
+    private Vector2D velocity = Vector2D.ZERO;
+    private double mass;
+
+    public PushBox(Position center, double halfWidth, double halfHeight, double mass) {
         super(center, halfWidth, halfHeight);
+        this.mass = mass;
     }
 
     @Override
-    public void push() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    public void push(Vector2D v) {
+        Vector2D dv = new Vector2D(v.x() / mass, v.y() / mass);
+        velocity = velocity.add(dv);
+    }
+
+    @Override
+    public void move(Vector2D delta) {
+        this.center = new Position(this.center.x() + delta.x(), this.center.y() + delta.y());
+    }
+
+    public void update(double dt) {
+        move(velocity.multiply(dt));
+        velocity.multiply(FRICTION);
     }
 
 }
