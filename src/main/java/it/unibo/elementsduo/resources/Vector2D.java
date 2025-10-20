d96cc2ff9bee94e83af293fcc42ea545843bd430
christian <kadiuchristian8@gmail.com>
2025-10-20 21:30:03 +0200

    change Vector2D class in record and add methods

--- src/main/java/it/unibo/elementsduo/resources/Vector2D.java
+++ src/main/java/it/unibo/elementsduo/resources/Vector2D.java
@@ -2,25 +2,38 @@ package it.unibo.elementsduo.resources;
 
 // class to represent a vector 
 
-public class Vector2D {
-    private final double x;
-    private final double y;
-
-    public Vector2D(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double getX() {
-        return this.x;
-    }
-
-    public double getY() {
-        return this.y;
-    }
+public record Vector2D(double x, double y) {
+    public static final Vector2D ZERO = new Vector2D(0, 0);
 
     public Vector2D add(Vector2D v) {
-        return new Vector2D(this.x + v.getX(), this.y + v.getY());
+        return new Vector2D(this.x + v.x, this.y + v.y);
+    }
+
+    public Vector2D subtract(Vector2D v) {
+        return new Vector2D(this.x - v.x, this.y - v.y);
+    }
+
+    public Vector2D multiply(double k) {
+        return new Vector2D(this.x * k, this.y * k);
+    }
+
+    public Vector2D divide(double k) {
+        if (k == 0.0)
+            throw new ArithmeticException("Division by zero");
+        return new Vector2D(this.x / k, this.y / k);
+    }
+
+    public double dot(Vector2D v) {
+        return this.x * v.x + this.y * v.y;
+    }
+
+    public double length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    public Vector2D normalize() {
+        double len = length();
+        return (len == 0.0) ? ZERO : new Vector2D(this.x / len, this.y / len);
     }
 
 }
