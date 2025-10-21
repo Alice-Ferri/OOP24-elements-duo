5ec06d3792e2c433389abce6551521102994fedf
christian <kadiuchristian8@gmail.com>
2025-10-21 14:27:52 +0200

    use Set of Levers to prevent repeated toggles during continuous collision

--- src/main/java/it/unibo/elementsduo/model/collisions/core/impl/handlers/LeverActivationHandler.java
+++ src/main/java/it/unibo/elementsduo/model/collisions/core/impl/handlers/LeverActivationHandler.java
@@ -1,5 +1,8 @@
 package it.unibo.elementsduo.model.collisions.core.impl.handlers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import it.unibo.elementsduo.model.collisions.core.api.Collidable;
 import it.unibo.elementsduo.model.collisions.core.api.CollisionHandler;
 import it.unibo.elementsduo.model.collisions.core.api.CollisionInformations;
@@ -7,6 +10,8 @@ import it.unibo.elementsduo.model.obstacles.InteractiveObstacles.impl.Lever;
 import it.unibo.elementsduo.model.player.api.Player;
 
 public class LeverActivationHandler implements CollisionHandler {
+
+    private Set<Lever> activeLevers = new HashSet<>();
 
     @Override
     public boolean canHandle(Collidable a, Collidable b) {
@@ -16,23 +21,25 @@ public class LeverActivationHandler implements CollisionHandler {
     @Override
     public void handle(CollisionInformations c) {
         Player player = null;
-        Collidable trigger = null;
+        Lever trigger = null;
         Collidable a = c.getObjectA();
         Collidable b = c.getObjectB();
 
         if (a instanceof Player) {
             player = (Player) a;
-            trigger = b;
+            trigger = (Lever) b;
         } else if (b instanceof Player) {
             player = (Player) b;
-            trigger = a;
+            trigger = (Lever) a;
         }
 
         if (player == null || trigger == null)
             return;
 
-        if (trigger instanceof Lever lever)
-            lever.toggle();
+        if (!this.activeLevers.contains(trigger)) {
+            trigger.toggle();
+            activeLevers.add(trigger);
+        }
     }
 
 }
