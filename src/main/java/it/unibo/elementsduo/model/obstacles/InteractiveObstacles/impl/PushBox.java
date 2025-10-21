58f66c7f158726a5471d52dcba6323ecd737454f
christian <kadiuchristian8@gmail.com>
2025-10-21 21:25:45 +0200

    implemented PushBox class and its updating logic

--- src/main/java/it/unibo/elementsduo/model/obstacles/InteractiveObstacles/impl/PushBox.java
+++ src/main/java/it/unibo/elementsduo/model/obstacles/InteractiveObstacles/impl/PushBox.java
@@ -6,10 +6,15 @@ import it.unibo.elementsduo.resources.Vector2D;
 
 public class PushBox extends InteractiveObstacle implements Pushable {
 
-    private final static double FRICTION = 0.9;
+    private static final double GROUND_FRICTION = 0.75;
+    private static final double AIR_FRICTION = 0.98;
+    private static final double GRAVITY = 9.8;
+    private static final double MIN_VELOCITY_X = 0.02;
+    private static final double MAX_FALL_SPEED = 15;
 
     private Vector2D velocity = Vector2D.ZERO;
     private double mass;
+    private boolean onGround = false;
 
     public PushBox(Position center, double halfWidth, double halfHeight, double mass) {
         super(center, halfWidth, halfHeight);
@@ -18,18 +23,47 @@ public class PushBox extends InteractiveObstacle implements Pushable {
 
     @Override
     public void push(Vector2D v) {
-        Vector2D dv = new Vector2D(v.x() / mass, v.y() / mass);
-        velocity = velocity.add(dv);
+        Vector2D orizontal = new Vector2D(v.x(), 0);
+        Vector2D accel = new Vector2D(orizontal.x() / mass, orizontal.y() / mass);
+        this.velocity = this.velocity.add(accel);
     }
 
     @Override
     public void move(Vector2D delta) {
-        this.center = new Position(this.center.x() + delta.x(), this.center.y() + delta.y());
+        this.center = this.center.add(delta);
     }
 
     public void update(double dt) {
+        if (!onGround) { // gravity
+            velocity = velocity.add(new Vector2D(0, GRAVITY * dt));
+        }
+
         move(velocity.multiply(dt));
-        velocity.multiply(FRICTION);
+        if (onGround) {
+            velocity = new Vector2D(velocity.x() * GROUND_FRICTION, velocity.y());
+        } else {
+            velocity = new Vector2D(velocity.x() * AIR_FRICTION, velocity.y());
+        }
+
+        if (Math.abs(velocity.x()) < MIN_VELOCITY_X) {
+            velocity = new Vector2D(0, velocity.y());
+        }
+
+        if (velocity.y() > MAX_FALL_SPEED) {
+            velocity = new Vector2D(velocity.x(), MAX_FALL_SPEED);
+        }
+    }
+
+    public void setOnGround(boolean state) {
+        this.onGround = state;
+    }
+
+    public boolean isOnGround() {
+        return this.onGround;
+    }
+
+    public Vector2D getVelocity() {
+        return this.velocity;
     }
 
 }
