53d0aaf71a10f9243278e3c94e765f62cfccb08d
Leonardo <st10151@iismarconipieralisi.it>
2025-10-23 08:21:17 +0200

    adj of main controller

--- src/main/java/it/unibo/elementsduo/application/Elements.java
+++ src/main/java/it/unibo/elementsduo/application/Elements.java
@@ -4,12 +4,9 @@ import it.unibo.elementsduo.controller.impl.MainControllerImpl;
 
 public class Elements {
     public static void main(String[] args) {
-        /*
-        MapLoader loader = new MapLoader(new obstacleFactory(),new EnemyFactoryImpl());
-        Level level = loader.loadLevel(1);
-        SwingUtilities.invokeLater(() -> new GameFrame(level));*/
 
-        new MainControllerImpl().start();
+        new MainControllerImpl().startApp();
+        
     }
 }
 
