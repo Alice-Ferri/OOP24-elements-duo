5b1e229e5aa4bb1b7951c6921930bf96bdf859e6
Alice-Ferri <alice.ferri8@studio.unibo.it>
2025-10-24 20:09:53 +0200

    class needed

---
+++ src/main/java/it/unibo/elementsduo/model/collisions/hitbox/impl/HitBoxImpl.java
@@ -0,0 +1,39 @@
+package it.unibo.elementsduo.model.collisions.hitbox.impl;
+
+import it.unibo.elementsduo.model.collisions.hitbox.api.HitBox;
+import it.unibo.elementsduo.resources.Position;
+
+public class HitBoxImpl implements HitBox {
+
+    private final Position center;
+    private final double height;
+    private final double width;
+
+    public HitBoxImpl(Position center, double height, double width) {
+        this.center = center;
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public Position getCenter() {
+        return this.center;
+    }
+
+    @Override
+    public double getHalfHeight() {
+        return this.height / 2.0;
+    }
+
+    @Override
+    public double getHalfWidth() {
+        return this.width / 2.0;
+    }
+
+    @Override
+    public boolean intersects(HitBox otherHitBox) {
+        return Math.abs(center.x() - otherHitBox.getCenter().x()) <= otherHitBox.getHalfWidth() + getHalfWidth()
+                && Math.abs(center.y() - otherHitBox.getCenter().y()) <= otherHitBox.getHalfHeight() + getHalfHeight();
+    }
+
+}
