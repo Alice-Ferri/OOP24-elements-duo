e3a3dec6300febf8df4ad0b81f4679d25996b5b7
christian <kadiuchristian8@gmail.com>
2025-10-24 11:35:38 +0200

    change return type

--- src/main/java/it/unibo/elementsduo/model/obstacles/StaticObstacles/impl/obstacleFactory.java
+++ src/main/java/it/unibo/elementsduo/model/obstacles/StaticObstacles/impl/obstacleFactory.java
@@ -2,6 +2,7 @@ package it.unibo.elementsduo.model.obstacles.StaticObstacles.impl;
 
 import it.unibo.elementsduo.model.collisions.hitbox.api.HitBox;
 import it.unibo.elementsduo.model.obstacles.StaticObstacles.api.Obstacle;
+import it.unibo.elementsduo.model.obstacles.StaticObstacles.api.StaticObstacle;
 import it.unibo.elementsduo.model.obstacles.StaticObstacles.impl.HazardObs.greenPool;
 import it.unibo.elementsduo.model.obstacles.StaticObstacles.impl.HazardObs.lavaPool;
 import it.unibo.elementsduo.model.obstacles.StaticObstacles.impl.HazardObs.waterPool;
@@ -9,7 +10,7 @@ import it.unibo.elementsduo.model.obstacles.StaticObstacles.impl.solid.Floor;
 import it.unibo.elementsduo.model.obstacles.StaticObstacles.impl.solid.Wall;
 
 public class obstacleFactory {
-    public Obstacle createObstacle(final obstacleType.type type, final HitBox hitbox) {
+    public StaticObstacle createObstacle(final obstacleType.type type, final HitBox hitbox) {
         switch (type) {
             case WATER_POOL:
                 return new waterPool(hitbox);
