389f39c404735fe471ac4e55774b2038e6fc489d
Leonardo <st10151@iismarconipieralisi.it>
2025-10-26 16:40:50 +0100

    subcontroller interface

---
+++ src/main/java/it/unibo/elementsduo/controller/subcontroller/impl/HomeController.java
@@ -0,0 +1,28 @@
+package it.unibo.elementsduo.controller.subcontroller.impl;
+
+
+import javax.swing.JPanel;
+
+import it.unibo.elementsduo.controller.subcontroller.api.Controller;
+
+public class HomeController implements Controller{
+
+    @Override
+    public void activate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'activate'");
+    }
+
+    @Override
+    public void deactivate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deactivate'");
+    }
+
+    @Override
+    public JPanel getPanel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPanel'");
+    }
+    
+}
