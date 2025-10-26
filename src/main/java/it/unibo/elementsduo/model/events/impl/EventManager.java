6c5bbe8e439e29e79bf0c8b54a4ebb1d8479d1b9
christian <kadiuchristian8@gmail.com>
2025-10-26 21:46:50 +0100

    change EventManager to implement logic of pairing listeners with the events they're interested on

--- src/main/java/it/unibo/elementsduo/model/events/impl/EventManager.java
+++ src/main/java/it/unibo/elementsduo/model/events/impl/EventManager.java
@@ -1,16 +1,23 @@
 package it.unibo.elementsduo.model.events.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import it.unibo.elementsduo.model.events.api.Event;
 import it.unibo.elementsduo.model.events.api.EventListener;
 
 public class EventManager {
-    private List<EventListener> Listeners = new ArrayList<>();
+    private Map<Class<? extends Event>, List<EventListener>> Listeners = new HashMap<>();
+
+    public void subscribe(Class<? extends Event> eventType, EventListener listener) {
+        this.Listeners.computeIfAbsent(eventType, k -> new ArrayList<>()).add(listener);
+    }
 
     public void notify(Event event) {
-        for (EventListener listener : Listeners) {
+        final List<EventListener> evListeners = this.Listeners.get(event.getClass());
+        for (EventListener listener : evListeners) {
             listener.onEvent(event);
         }
     }
