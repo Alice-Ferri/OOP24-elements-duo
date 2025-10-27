50c3e5ff395bf088904059e6a75b1c866355e13e
christian <kadiuchristian8@gmail.com>
2025-10-27 15:59:08 +0100

    add EnemyDiedEvent

---
+++ src/main/java/it/unibo/elementsduo/model/events/impl/EnemyDiedEvent.java
@@ -0,0 +1,17 @@
+package it.unibo.elementsduo.model.events.impl;
+
+import it.unibo.elementsduo.model.Enemies.Enemy;
+import it.unibo.elementsduo.model.events.api.Event;
+
+public class EnemyDiedEvent implements Event {
+
+    Enemy enemy;
+
+    public EnemyDiedEvent(final Enemy enemy) {
+        this.enemy = enemy;
+    }
+
+    public Enemy getEnemy() {
+        return this.enemy;
+    }
+}
