dd1366185b6bd98f3e224ab3a8399ec0afbea659
Leonardo <st10151@iismarconipieralisi.it>
2025-10-27 12:09:54 +0100

    check Spawns

--- src/main/java/it/unibo/elementsduo/model/map/mapvalidator/impl/MapValidatorImpl.java
+++ src/main/java/it/unibo/elementsduo/model/map/mapvalidator/impl/MapValidatorImpl.java
@@ -3,6 +3,10 @@ package it.unibo.elementsduo.model.map.mapvalidator.impl;
 import it.unibo.elementsduo.model.map.level.api.Level;
 import it.unibo.elementsduo.model.map.mapvalidator.api.InvalidMapException;
 import it.unibo.elementsduo.model.map.mapvalidator.api.MapValidator;
+import it.unibo.elementsduo.model.obstacles.StaticObstacles.impl.exit.fireExit;
+import it.unibo.elementsduo.model.obstacles.StaticObstacles.impl.exit.waterExit;
+import it.unibo.elementsduo.model.obstacles.StaticObstacles.impl.spawn.fireSpawn;
+import it.unibo.elementsduo.model.obstacles.StaticObstacles.impl.spawn.waterSpawn;
 
 public class MapValidatorImpl implements MapValidator{
 
@@ -22,7 +26,18 @@ public class MapValidatorImpl implements MapValidator{
     }
 
     private void checkSpawnsAndExits(final Level level) throws InvalidMapException{
-
+        if (level.getObstaclesByClass(fireSpawn.class).size() != 1) {
+            throw new InvalidMapException("La mappa deve avere esattamente 1 fire spawn.");
+        }
+        if (level.getObstaclesByClass(waterSpawn.class).size() != 1) {
+            throw new InvalidMapException("La mappa deve avere esattamente 1 water spawn.");
+        }
+        if (level.getObstaclesByClass(fireExit.class).size() != 1) {
+            throw new InvalidMapException("La mappa deve avere esattamente 1 fire exit.");
+        }
+        if (level.getObstaclesByClass(waterExit.class).size() != 1) {
+            throw new InvalidMapException("La mappa deve avere esattamente 1 water exit.");
+        }
     }
 
     private void checkBoundaries(final Level level) throws InvalidMapException{
