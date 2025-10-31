a7a9b84bba2568ca385f4bc2a906851be5684b83
Leonardo <st10151@iismarconipieralisi.it>
2025-10-31 02:30:39 +0100

    javadoc and checkstyle for gamestate

--- src/main/java/it/unibo/elementsduo/model/gameentity/api/Updatable.java
+++ src/main/java/it/unibo/elementsduo/model/gameentity/api/Updatable.java
@@ -1,6 +1,15 @@
 package it.unibo.elementsduo.model.gameentity.api;
 
+/**
+ * Represents a game object that can be updated over time,
+ * such as for movement or animation.
+ */
 @FunctionalInterface
 public interface Updatable {
+    /**
+     * Updates the state of the object.
+     *
+     * @param deltatime The time elapsed since the last frame/update, in seconds.
+     */
     void update(double deltatime);
 }
