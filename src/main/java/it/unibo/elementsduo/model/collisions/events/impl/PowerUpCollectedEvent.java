6847ad4a68ee174623d10faafd2fddee7cdf2bee
christian <kadiuchristian8@gmail.com>
2025-11-02 23:57:21 +0100

    add puwerups events

---
+++ src/main/java/it/unibo/elementsduo/model/collisions/events/impl/PowerUpCollectedEvent.java
@@ -0,0 +1,8 @@
+package it.unibo.elementsduo.model.collisions.events.impl;
+
+import it.unibo.elementsduo.model.collisions.events.api.Event;
+import it.unibo.elementsduo.model.player.api.Player;
+import it.unibo.elementsduo.model.powerups.api.PowerUpType;
+
+public record PowerUpCollectedEvent(Player player, PowerUpType type) implements Event {
+}
